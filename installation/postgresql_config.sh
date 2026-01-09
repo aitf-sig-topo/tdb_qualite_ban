7b49584851b94150c01e156f43847c0e96f459e6
Maël REBOUX <m.reboux@rennesmetropole.fr>
2026-01-09 17:55:49 +0100

    Update postgresql_config.sh
    PG_HBA_CONF
    DROP and RECREATE

--- installation/postgresql_config.sh
+++ installation/postgresql_config.sh
@@ -11,14 +11,27 @@ PG_CONF="/etc/postgresql/*/main/postgresql.conf"
 PG_SERVICE="postgresql"
 PG_PASS_FILE="$HOME/.pgpass"
 
+
 # 1. Créer le rôle 'aitf_admin' avec mot de passe et droits superuser
 echo "Création du rôle '$PG_ROLE'..."
 sudo -u $PG_USER psql -c "CREATE ROLE $PG_ROLE WITH SUPERUSER LOGIN PASSWORD '$PG_PASSWORD';"
 
 # 2.1 Créer la base de données 'ban' et assigner le rôle 'aitf_admin' comme propriétaire
 echo "Création de la base de données '$PG_DB'..."
+
+# on tue les connexions en cours
+sudo -u $PG_USER psql -c "
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'ban'
+AND leader_pid IS NULL;"
+
+# on supprime
 sudo -u $PG_USER psql -c "DROP DATABASE $PG_DB ;"
+
+# on recrée
 sudo -u $PG_USER psql -c "CREATE DATABASE $PG_DB OWNER $PG_ROLE;"
+
 
 # 2.2 Les extensions
 sudo -u $PG_USER psql -d $PG_DB -c "CREATE EXTENSION postgis;" || true
@@ -26,7 +39,7 @@ sudo -u $PG_USER psql -d $PG_DB -c "CREATE EXTENSION postgis;" || true
 
 # 3. Modifier le fichier pg_hba.conf pour autoriser l'accès depuis n'importe où
 echo "Modification de $PG_HBA_CONF pour autoriser l'accès..."
-echo "host    ban             aitf_admin      0.0.0.0/0               scram-sha-256" | sudo tee -a $PG_HBA_CONF
+echo "host    all             aitf_admin      0.0.0.0/0               scram-sha-256" | sudo tee -a $PG_HBA_CONF
 
 # 4. Modifier le fichier postgresql.conf pour écouter sur toutes les adresses
 echo "Modification de $PG_CONF pour écouter sur toutes les adresses..."
