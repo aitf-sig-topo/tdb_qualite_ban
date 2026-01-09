c37b4481818a9d35331c016b70a8ab473a93333b
Maël REBOUX <m.reboux@rennesmetropole.fr>
2026-01-09 17:26:35 +0100

    Update serveur_configuration.sh

--- installation/serveur_configuration.sh
+++ installation/serveur_configuration.sh
@@ -2,10 +2,18 @@
 
 
 # Créer un groupe dédié
+echo "Création du groupe aitf"
 sudo groupadd aitf
 
-# Ajouter les utilisateurs au groupe
-sudo usermod -aG aitf a_user
+# Demande à l'utilisateur de saisir une liste de comptes séparés par des espaces
+read -p "Entrez la liste des utilisateurs à ajouter au groupe 'aitf' (séparés par des espaces) : " users
+
+# Boucle sur chaque utilisateur
+for user in $users; do
+    echo "Ajout de $user au groupe aitf..."
+    sudo usermod -aG aitf "$user"
+    echo "Fait pour $user."
+done
 
 
 # création du répertoire du projet
@@ -15,8 +23,11 @@ sudo mkdir /srv/aitf
 sudo chown -R :aitf /srv/aitf
 sudo chmod -R 775 /srv/aitf
 
+echo "clone du projet..."
 
 # git clone du projet
 cd /srv/aitf/
 git clone https://github.com/aitf-sig-topo/tdb_qualite_ban.git
+
+echo "fait"
 
