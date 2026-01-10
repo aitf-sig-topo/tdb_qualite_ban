a8f4687f7c60e98836fbebaa322b142daf9f8721
Maël REBOUX <m.reboux@rennesmetropole.fr>
2026-01-10 15:05:53 +0100

    création des indicateurs : gestion dpt ou france entière

--- creer_table_bal_indicateurs.sh
+++ creer_table_bal_indicateurs.sh
@@ -1,11 +1,61 @@
-# Crée la table des indicateurs bal en requêtant dans la table bal_brute et joignant avec la table des contours des communes.
+#!/bin/sh
+set -e
+
+echo ""
+echo "[$(date '+%d/%m/%Y %H:%M:%S')]"
+echo ""
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  Calcul des indicateurs de qualité"
+echo ""
 
 # lecture du fichier de configuration
-. config.sh
+. ./config.sh
 
-echo "[$(date '+%d/%m/%Y %H:%M:%S')] DEBUT Création table indicateurs BAL"
+# repertoire de travail
+BASEDIR=$(cd $(dirname $0) && pwd)
+
+
+# Initialisation de la variable BAL_TO_TREAT
+BAL_TO_TREAT="france"
+
+# Vérification des arguments
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -dpt)
+            if [ -n "$2" ]; then
+                BAL_TO_TREAT="$2"
+                shift 2
+            else
+                echo "Erreur : L'argument pour -dpt est manquant."
+                exit 1
+            fi
+            ;;
+        *)
+            echo "Option inconnue : $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$BAL_TO_TREAT" = "france" ]; then
+    echo "On va travailler sur la France entière"
+else
+    echo "On va travailler sur le département $BAL_TO_TREAT"
+fi
+
+
+echo ""
 
 # script SQL de creation des indicateur
-eval "psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USERNAME -d $POSTGRES_DB -f ./sql/create_table_bal_indicateurs.sql"
+PSQL_CMD="$PSQL_BASE_CMD -f ./sql/create_table_bal_indicateurs.sql"
+echo "$PSQL_CMD"
+eval "$PSQL_CMD"
 
-echo "[$(date '+%d/%m/%Y %H:%M:%S')] FIN Création table indicateurs BAL"
+
+echo ""
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo "  FINI"
+echo ""
+echo "[$(date '+%d/%m/%Y %H:%M:%S')]"
+echo ""
+echo ""
