845c308044c31d542f30ac92282a270e0636ae1e
Maël REBOUX <m.reboux@rennesmetropole.fr>
2026-01-14 18:46:12 +0100

    suppr index bal_brute_commune_nom_idx

--- sql/create_indexes_bal_brute.sql
+++ sql/create_indexes_bal_brute.sql
@@ -1,7 +1,6 @@
 
 
 CREATE INDEX bal_brute_commune_insee_idx ON bal_brute (commune_insee);
-CREATE INDEX bal_brute_commune_nom_idx ON bal_brute (commune_nom);
 CREATE INDEX bal_brute_source_idx ON bal_brute (source);
 CREATE INDEX bal_brute_commune_date_der_maj_idx ON bal_brute (date_der_maj);
 
